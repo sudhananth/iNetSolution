8f7222f13159e56d1c07fa4866ac7b20823da522
sudhananth <33347962+sudhananth@users.noreply.github.com>
2017-12-09 19:30:18 +0530

    new Design

---
+++ New/script.js
@@ -0,0 +1,62 @@
+// create the module and name it InetSolution
+var inet = angular.module('inet', ['ngRoute', 'ngSanitize']);
+inet.run(function ($rootScope, $http) {
+    $rootScope.courses = new Array;
+    $http({ method: 'GET', url: 'data.json' }).then(function (data) {
+        $rootScope.courses = data.data.courses;
+    }, function (error) {
+        alert('Error');
+    });
+});
+// configure our routes
+inet.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/home', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        // route for the about page
+        .when('/courses', {
+            templateUrl: 'pages/courses.html',
+            controller: 'coursesController'
+        })
+         .when('/courseDetail/:id', {
+             templateUrl: 'pages/courseDetails.html',
+             controller: 'coursesController'
+         })
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+inet.controller('mainController', function ($scope, $http, $rootScope, $location) {
+    // create a message to display in our view
+    $scope.courses = $rootScope.courses;
+    //$scope.navigate = function (path) {
+    //    if (path.split('!').length > 0)
+    //        $location.path(path.split('!')[1]);
+    //}
+
+    $scope.year = new Date().getFullYear(); 
+});
+
+
+inet.controller('coursesController', function ($scope, $route, $rootScope) {
+    var id = $route.current.params.id;
+    $scope.courses = $rootScope.courses;
+    if (id)
+        $scope.course = $scope.courses[parseInt(id[1])];
+
+});
+
+inet.controller('contactController', function ($scope, $rootScope) {
+});
